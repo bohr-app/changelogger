bd9adb3005f03b018e6bc03a66850f21ba831ff9
Don Gerrard <->
2018-12-12 15:21:49 +0100

    Stopped tracking out

--- out/errors/cleaned-empty.function.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-function cleanedEmpty() {
-    console.log('INFO:', 'Log entries without a meaningful update message have been removed.');
-}
-exports.cleanedEmpty = cleanedEmpty;
