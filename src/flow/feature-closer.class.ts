97ef7a08801f55685736996d200d291847ba946a
Don Gerrard <don.gerrard@outlook.com>
2018-12-13 21:23:40 +0100

    FeatureCloser now merges into develop and removes localfeature branch

--- src/flow/feature-closer.class.ts
+++ src/flow/feature-closer.class.ts
@@ -1,5 +1,4 @@
 import { FlowBase } from '@bohr/changelogger/flow/flow-base.class';
-import { Committer } from '@bohr/changelogger/git-manager/committer.class';
 import * as cmd from 'node-cmd';
 import { promisify } from 'util';
 
@@ -7,14 +6,13 @@ export class FeatureCloser extends FlowBase {
 
   public async close(): Promise<void> {
     this.init();
+
     if (!this.isCurrentAFeature())
       return;
 
     await this.checkoutToDevelop();
     await this.mergeFeatureOnDevelop();
-    // await this.callCommitter();
-    this.checkStatus();
-    // await this.deleteFeatureBranch();
+    await this.deleteFeatureBranch();
   }
 
   private isCurrentAFeature(): boolean {
@@ -26,33 +24,11 @@ export class FeatureCloser extends FlowBase {
   }
 
   private async mergeFeatureOnDevelop(): Promise<void> {
-    try {
-      await promisify(cmd.get)(`git merge ${this.currentBranch}`);
-    } catch (err) {
-      console.error('Err merge:', err);
-    }
-  }
-
-  private async rebase(): Promise<void> {
-    await promisify(cmd.get)(`git rebase ${this.currentBranch} develop`);
-  }
-
-  private async callCommitter(): Promise<void> {
-    const message = `Merged branch ${this.currentBranch} into develop`;
-    try {
-      await new Committer(undefined, message).commit();
-    } catch (err) {
-      console.error('Err 1', err);
-    }
+    await promisify(cmd.get)(`git merge ${this.currentBranch}`);
   }
 
   private async deleteFeatureBranch(): Promise<void> {
-    try {
-      await this.git.removeLocalBranch(this.currentBranch);
-    } catch (err) {
-      console.error('Err 2', err);
-    }
-
+    await this.git.removeLocalBranch(this.currentBranch);
   }
 
 }
