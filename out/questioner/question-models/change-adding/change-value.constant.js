ca391e73c08dc5504086a4a984b19d69c4c02350
Don Gerrard <don.gerrard@outlook.com>
2018-12-13 22:02:24 +0100

    Stopped tracking out

--- out/questioner/question-models/change-adding/change-value.constant.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.CHANGE_VALUE = {
-    type: 'input',
-    name: 'value',
-    message: 'Change content:'
-};
--- out/renderers/renderer-starter.function.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var md_maker_class_1 = require('./mark-down/md-maker.class');
-function rendererStarter(type) {
-    new md_maker_class_1.MdMaker().make();
-}
-exports.rendererStarter = rendererStarter;
