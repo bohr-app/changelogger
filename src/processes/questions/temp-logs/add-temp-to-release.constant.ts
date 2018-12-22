9c4ff0f9ce275e348c37db4e291182e747ddc639
Don Gerrard <don.gerrard@outlook.com>
2018-12-22 16:47:20 +0100

    Removed answers from confirm question

--- src/processes/questions/temp-logs/add-temp-to-release.constant.ts
+++ src/processes/questions/temp-logs/add-temp-to-release.constant.ts
@@ -3,9 +3,5 @@ import { Question } from 'inquirer';
 export const ADD_TEMP_TO_RELEASE: Question = {
   type: 'confirm',
   name: 'choice',
-  message: 'Temp logs found in changelog.json, add them to this release?',
-  choices: [
-    { name: 'Yes', value: 'yes' },
-    { name: 'No', value: 'no' }
-  ]
+  message: 'Temp logs found in changelog.json, add them to this release?'
 };
